b21b172b44aebf0f07048d6b60d989f2a67d2314
raspberry <rlcruz324@csu.fullerton.edu>
2025-12-08 04:40:08 -0800

    modified:   backend/src/controllers/propertyControllers.ts 	modified:   notes.txt

--- backend/src/controllers/propertyControllers.ts
+++ backend/src/controllers/propertyControllers.ts
@@ -237,23 +237,23 @@ export const createProperty = async (
     } = req.body;
 
     //Uploads each photo to S3 and collects their URLs.
-    // const photoUrls = await Promise.all(
-    //   files.map(async (file) => {
-    //     const uploadParams = {
-    //       Bucket: process.env.S3_BUCKET_NAME!,
-    //       Key: `properties/${Date.now()}-${file.originalname}`,
-    //       Body: file.buffer,
-    //       ContentType: file.mimetype,
-    //     };
+    const photoUrls = await Promise.all(
+      files.map(async (file) => {
+        const uploadParams = {
+          Bucket: process.env.S3_BUCKET_NAME!,
+          Key: `properties/${Date.now()}-${file.originalname}`,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+        };
 
-    //     const uploadResult = await new Upload({
-    //       client: s3Client,
-    //       params: uploadParams,
-    //     }).done();
+        const uploadResult = await new Upload({
+          client: s3Client,
+          params: uploadParams,
+        }).done();
 
-    //     return uploadResult.Location;
-    //   })
-    // );
+        return uploadResult.Location;
+      })
+    );
 
     //Performs geocoding using OpenStreetMap Nominatim API.
     const geocodingUrl = `https://nominatim.openstreetmap.org/search?${new URLSearchParams(
@@ -292,7 +292,7 @@ export const createProperty = async (
     const newProperty = await prisma.property.create({
       data: {
         ...propertyData,
-        //photoUrls,
+        photoUrls,
         locationId: location.id,
         managerCognitoId,
         amenities:
