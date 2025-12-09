36185562b95840ab4bea1b2a83c53a46d3308fcd
raspberry <rlcruz324@csu.fullerton.edu>
2025-12-09 03:05:05 -0800

    modified:   frontend/next.config.ts 	modified:   notes.txt

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,12 +9,18 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      // {
+      //   protocol: "https",
+      //   hostname: "*.amazonaws.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
       {
         protocol: "https",
-        hostname: "*.amazonaws.com",
+        hostname: "lucky-star-s3-images.s3.us-west-2.amazonaws.com",
         port: "",
         pathname: "/**",
-      }
+      },
     ],  
   },
 };
